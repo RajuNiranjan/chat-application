b4515acb812800c0df8a81572eadad2c53d76751
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-10-03 22:09:11 +0530

    fetched user globally

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import { useDispatch, useSelector } from "react-redux";
 import Home from "./pages/home/index";
 import LogIn from "./pages/login/index";
 import Register from "./pages/register/index";
@@ -7,16 +8,42 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import { useEffect } from "react";
+import { fetchUser } from "./redux/reducers/auth.reducer";
+import { AppDispatch, RootState } from "./redux/store";
 
 const App = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const { user } = useSelector((state: RootState) => state.auth);
+  console.log("user", user);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      dispatch(fetchUser(token));
+    }
+  }, [dispatch]);
+
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="*" element={<Navigate to="/login" />} />
+          <Route
+            path="/"
+            element={user ? <Home /> : <Navigate to="/login" />}
+          />
+
+          <Route
+            path="/login"
+            element={!user ? <LogIn /> : <Navigate to="/" />}
+          />
+
+          <Route
+            path="/register"
+            element={!user ? <Register /> : <Navigate to="/" />}
+          />
+
+          <Route path="*" element={<Navigate to={user ? "/" : "/login"} />} />
         </Routes>
       </Router>
     </>
--- client/src/redux/reducers/auth.reducer.ts
+++ client/src/redux/reducers/auth.reducer.ts
@@ -1,16 +1,39 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
+
+interface User {
+  _id: string;
+  userName: string;
+  fullName: string;
+  gender: string;
+  profilePic: string;
+}
 
 interface AuthState {
   loading: boolean;
   error: null | string;
   token: string | null;
+  user: null | User;
 }
 
 const initialState: AuthState = {
   loading: false,
   error: null,
   token: null,
+  user: null,
 };
+
+export const fetchUser = createAsyncThunk(
+  "user/fetchUser",
+  async (token: string) => {
+    const res = await axios.get("/api/me", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return res.data.user;
+  }
+);
 
 const authSlice = createSlice({
   name: "auth",
@@ -28,6 +51,20 @@ const authSlice = createSlice({
       state.token = action.payload;
       state.error = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchUser.fulfilled, (state, action: PayloadAction<User>) => {
+        state.user = action.payload;
+      })
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      .addCase(fetchUser.rejected, (state, action: PayloadAction<any>) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
   },
 });
 
